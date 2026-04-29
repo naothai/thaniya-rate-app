d74a214c3d4d7a4932f53d602620442736cd65a9
azuma naoki <azumanaoki@azumanoMacBook-Pro.local>
2026-04-30 02:28:38 +0700

    fix: disable service worker cache

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,15 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", event => {
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
   self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  // キャッシュしない。ブラウザ通常通信に任せる。
+  return;
 });
